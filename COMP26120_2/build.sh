476e8f14b6b1791ce5b2fd631f885202fb86dbb0
Todd Davies <todd434@gmail.com>
2015-05-10 12:48:19 +0100

    Finished the penultimate 261 lecture

--- COMP26120_2/build.sh
+++ COMP26120_2/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+hostname="todddavies.co.uk"
+hostport=22
+remoteuser="root"
+
 compileall=
 remote=
+fastArgs=""
 
 function usage
 {
-  echo "usage: build.sh [-a -r ]"
+  echo "usage: build.sh [-a -r -f]"
+  echo "  -a (Re)Compile all diagrams, kindle versions etc (slow!)"
+  echo "  -r Compile on a remote server"
+  echo "  -f Compile quickly (maybe taking shortcuts along the way)"
 }
 
 while [ "$1" != "" ]; do
@@ -13,6 +21,8 @@ while [ "$1" != "" ]; do
         -a | --compileall )     compileall=1
                                 ;;
         -r | --remote )         remote=1
+                                ;;
+        -f | --fast )           fastArgs="\def\fastCompile{1} "
                                 ;;
         -h | --help )           usage
                                 exit
@@ -28,11 +38,11 @@ done;
 
 if [ "$remote" = 1 ]; then
   zip -r content.zip ./ -x *.git*
-  ssh -p 51995 root@95.138.180.152 'rm -rf ~/tmp/latex_build; mkdir -p ~/tmp/latex_build;'
-  scp -P 51995 content.zip root@95.138.180.152:~/tmp/latex_build
-  ssh -p 51995 root@95.138.180.152 "cd ~/tmp/latex_build/;unzip content.zip;rm content.zip;./build.sh -n;zip content.zip ./*.pdf;"
+  ssh -p $hostport $remoteuser@$hostname 'rm -rf ~/tmp/latex_build; mkdir -p ~/tmp/latex_build;'
+  scp -P $hostport content.zip $remoteuser@$hostname:~/tmp/latex_build
+  ssh -p $hostport $remoteuser@$hostname "cd ~/tmp/latex_build/;unzip content.zip;rm content.zip;./build.sh -n;zip content.zip ./*.pdf;"
   rm content.zip
-  scp -P 51995 root@95.138.180.152:~/tmp/latex_build/content.zip ./content.zip
+  scp -P $hostport $remoteuser@$hostname:~/tmp/latex_build/content.zip ./content.zip
   unzip -o content.zip
   rm content.zip
 else
@@ -47,9 +57,10 @@ else
     done;
     wait;
   fi
-  pdflatex --shell-escape notes.tex &
+  # TODO: Make this take arguments
+  pdflatex -shell-escape "$fastArgs\input{notes.tex}" &
   if [ "$compileall" = "1" ]; then
-    pdflatex --shell-escape kindle.tex &
+    pdflatex -shell-escape "$fastArgs\input{kindle.tex}" &
   fi
   wait;
   # In case the Author field isn't set
